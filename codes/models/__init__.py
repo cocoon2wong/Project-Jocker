772dd7937c65904cac9f37d957a8c4a5812d1041
conghaowong <conghao_wong@icloud.com>
2022-07-18 10:34:44 +0800

    add linear model in `codes`

--- codes/models/__init__.py
+++ codes/models/__init__.py
@@ -1,11 +1,11 @@
 """
 @Author: Conghao Wong
-@Date: 2022-06-20 15:30:24
+@Date: 2022-07-15 20:17:39
 @LastEditors: Conghao Wong
-@LastEditTime: 2022-07-15 20:18:57
+@LastEditTime: 2022-07-15 20:17:41
 @Description: file content
 @Github: https://github.com/cocoon2wong
 @Copyright 2022 Conghao Wong, All Rights Reserved.
 """
 
-from . import args, basemodels, models, training
+from .__linear import Linear
---
+++ codes/models/__linear.py
@@ -0,0 +1,39 @@
+"""
+@Author: Conghao Wong
+@Date: 2022-07-15 20:13:07
+@LastEditors: Conghao Wong
+@LastEditTime: 2022-07-15 20:40:58
+@Description: file content
+@Github: https://github.com/cocoon2wong
+@Copyright 2022 Conghao Wong, All Rights Reserved.
+"""
+
+from ..args import BaseArgTable
+from ..basemodels import Model, layers
+from ..training import Structure
+
+
+class LinearModel(Model):
+    def __init__(self, Args: BaseArgTable, structure=None, *args, **kwargs):
+        super().__init__(Args, structure, *args, **kwargs)
+
+        self.linear = layers.LinearLayer(obs_frames=self.args.obs_frames,
+                                         pred_frames=self.args.pred_frames)
+
+    def call(self, inputs, training=None, *args, **kwargs):
+        trajs = inputs[0]
+        return self.linear.call(trajs)
+
+
+class Linear(Structure):
+    def __init__(self, terminal_args: list[str]):
+        super().__init__(terminal_args)
+
+        self.set_inputs('obs')
+        self.set_labels('pred')
+
+        self.args._set('epochs', 3)
+        self.args._set('use_maps', 0)
+
+    def create_model(self, *args, **kwargs) -> Model:
+        return LinearModel(self.args)
