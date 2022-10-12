0b57bfbe33d3b279de5d647a172120b8cf569fbc
conghaowong <conghao_wong@icloud.com>
2022-10-12 18:59:38 +0800

    speed up iou

--- codes/training/loss/__iou.py
+++ codes/training/loss/__iou.py
@@ -2,7 +2,7 @@
 @Author: Conghao Wong
 @Date: 2022-10-12 10:50:35
 @LastEditors: Conghao Wong
-@LastEditTime: 2022-10-12 13:53:25
+@LastEditTime: 2022-10-12 18:58:41
 @Description: file content
 @Github: https://github.com/cocoon2wong
 @Copyright 2022 Conghao Wong, All Rights Reserved.
@@ -24,8 +24,7 @@ def AIoU(outputs: list[tf.Tensor],
     if pred.ndim == 3:
         pred = pred[:, tf.newaxis, :, :]
 
-    K = pred.shape[1]
-    GT = tf.repeat(GT[:, tf.newaxis, :, :], K, axis=-3)
+    GT = GT[:, tf.newaxis, :, :]
 
     # (batch, K, steps)
     iou = __IoU_single_2Dbox(pred, GT)
@@ -55,17 +54,21 @@ def __IoU_single_2Dbox(box1: tf.Tensor, box2: tf.Tensor) -> tf.Tensor:
     :param GT: shape = (..., 4)
     """
 
-    # (0,   1,   2,   3,   4,   5,   6,   7  )
-    # (xl1, yl1, xr1, yrl, xl2, yl2, xr2, yr2)
-    dat = tf.concat([box1, box2], axis=-1)
+    # box1:
+    # (0,   1,   2,   3  )
+    # (xl1, yl1, xr1, yrl)
+
+    # box2:
+    # (0,   1,   2,   3  )
+    # (xl2, yl2, xr2, yr2)
 
     len1_x, len2_x, len_inter_x = \
-        __get_len_1Dbox(tf.gather(dat, [0, 2], axis=-1),
-                        tf.gather(dat, [4, 6], axis=-1))
+        __get_len_1Dbox(tf.gather(box1, [0, 2], axis=-1),
+                        tf.gather(box2, [0, 2], axis=-1))
 
     len1_y, len2_y, len_inter_y = \
-        __get_len_1Dbox(tf.gather(dat, [1, 3], axis=-1),
-                        tf.gather(dat, [5, 7], axis=-1))
+        __get_len_1Dbox(tf.gather(box1, [1, 3], axis=-1),
+                        tf.gather(box2, [1, 3], axis=-1))
 
     s_inter = len_inter_x * len_inter_y
     s_all = len1_x * len1_y + len2_x * len2_y
@@ -80,8 +83,15 @@ def __get_len_1Dbox(box1: tf.Tensor, box2: tf.Tensor):
     len1 = tf.abs(box1[..., 0] - box1[..., 1])
     len2 = tf.abs(box2[..., 0] - box2[..., 1])
 
-    x_sorted = tf.sort(tf.concat([box1, box2], axis=-1))
-    len_all = tf.abs(x_sorted[..., 0] - x_sorted[..., -1])
-    len_inter = tf.maximum(len1 + len2 - len_all, 0.0)
+    K = len1.shape[-2]
+    len2 = tf.repeat(len2, K, axis=-2)
+
+    len_all = [len1, len2]
+    for i in [0, 1]:
+        for j in [0, 1]:
+            len_all.append(box1[..., i] - box2[..., j])
+
+    len_max = tf.reduce_max(tf.abs(len_all), axis=0)
+    len_inter = tf.maximum(len1 + len2 - len_max, 0.0)
 
     return len1, len2, len_inter
